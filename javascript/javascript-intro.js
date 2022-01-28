0e1fdc4d4c33032a409efa2540ab2c4129ff721b
sanketcodekul <sanketbhosale.codekul@gmail.com>
2022-01-28 10:57:52 +0530

    Hoisting and Javascript Functions

---
+++ javascript/javascript-intro.js
@@ -0,0 +1,60 @@
+//alert("Hello, welcome to javascript")
+
+
+// let, const and var keywords
+
+//global scope, local/block scope and function scope
+
+// basic programming concepts - variables, functions, datatypes, conditional statements, iteration statements
+
+// hoisting in javascript 
+
+
+// https://www.figma.com/file/EWmzcVkd7qbP5Nf7iMvuqP/Trafalgar-Landing-Page?node-id=0%3A1
+
+// declaration 
+// initialization
+// using variable/function
+
+// var keyword variable is a global scope variable as well as function
+// let keyword variable is a block scope variable as function scope
+// const keyword variable 
+
+
+const PI = 3.14;
+
+//PI =3.14;
+
+console.log(PI)
+var num1;
+
+num1 = 200;
+
+
+num1 = 500;
+
+console.log(num1)
+
+if (true) {
+    var number = 100
+
+
+}
+
+console.log(number)
+
+function displayMessage() {
+    var fname = "Codekul"
+
+
+}
+
+//console.log(fname)
+
+
+displayMessage()
+
+console.log("Welcome to Codekul")
+
+
+
